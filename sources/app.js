a585c53756bd7c59295664c8c79a51fbc7633b20
Lucas Casotti <lucas.rookis@gmail.com>
2016-11-28 22:50:14 -0200

    Teste requisicao get com retorno JSON

--- sources/app.js
+++ sources/app.js
@@ -3,4 +3,13 @@ import express from 'express'
 const server = express();
 const port = process.env.PORT || 3000;
 
+server.get('/users', function(req, res) {
+  const users = [
+    {name: 'Darlan'},
+    {name: 'Lucas'}
+  ]
+  res.json(users);
+  console.log(req);
+})
+
 server.listen(port, () => console.log(`localhost:${port}`));
